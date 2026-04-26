8f8512ecac6815e2391dfe5407ce5c1a30356ea0
Vaibhav Datkhil <vaibhavdatkhil@gmail.com>
2026-04-26 10:21:17 +0530

    improved UI for CI test

--- app.py
+++ app.py
@@ -9,8 +9,15 @@ def index():
     if request.method == "POST":
         name = request.form["name"]
         email = request.form["email"]
-        registrations.append({"name": name, "email": email})
-    return render_template("index.html", data=registrations)
+        phone = request.form["phone"]   # NEW FIELD
+
+        registrations.append({
+            "name": name,
+            "email": email,
+            "phone": phone
+        })
+
+    return render_template("index.html", data=registrations, success=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
